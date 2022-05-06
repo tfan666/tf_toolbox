558b57691ca2aea87043cd98771e59919c7c3e5d
Tongtian Fan <franktongtim@gmail.com>
2022-05-06 13:49:56 -0400

    update

---
+++ ds_modules.py
@@ -0,0 +1,20 @@
+from sklearn.metrics import classification_report, brier_score_loss, log_loss, recall_score, precision_score, accuracy_score
+import sys
+
+
+def f2_score(y_true=None, y_pred=None, precision=None, recall=None, type='direct'):
+    """
+    Harmonic weighted mean of precision and recall, with
+    more weight on recall.
+    """
+    if type=='direct':
+        recall = recall_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+        f2_score = (1+ 2**2) / ((2**2/recall) + 1/precision)
+        return f2_score
+
+    if type=='indirect':
+        f2_score = (1+ 2**2) / ((2**2/recall) + 1/precision)
+        return f2_score
+    else:
+        sys.exit("type has to be one of 'direct' or 'indirect'.")
---
+++ viz_modules.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+import plotly
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+

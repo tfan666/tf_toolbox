5add93510d0af7c9c11e5ae992c448e8e5a18ec5
Tongtian Fan <Tongtian.Fan@cbsinteractive.com>
2022-06-01 18:07:31 -0400

    add solver

--- stats_modules.py
+++ stats_modules.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from scipy.linalg import solve
 
 
 
 
-def least_square(X, y):
-    beta = np.linalg.inv(X.T@X) @ X.T @y
+def least_square(X, y, use_solver=False):
+    if use_solver == False:
+        beta = np.linalg.inv(X.T@X) @ X.T @y
+    else:
+        beta = solve(a=X.T @ X, b=X.T @ y)
     beta_0 = (X @ beta).mean() - y.mean()
     return beta, beta_0
 
@@ -16,18 +20,25 @@ class linear_regression:
         self.y = y
         self.para = {}
         self.method = None
+        self.residuals = None
     
     def fit(self, method='OLS'):
         """
         This function fit the given data. Available methods:
-            - OLS: simple linear regression (Orindary Least Square)
+            - OLS: simple linear regression (Orindary Least Square) using matrix multplication
+            - OLS_solver: simple linear regression (Orindary Least Square) using linear solver
             - GD:Gradient Desecent
             - L1: Lasso Regression
             - L2: Ridge Regression
         """
+        # check fitting methods
         if method == 'OLS':
             
             beta, beta_0 = least_square(X=self.X, y=self.y)
+            self.para['coef'] = beta
+            self.para['intercept'] = beta_0
+        elif method == 'OLS_solver':
+            beta, beta_0 = least_square(X=self.X, y=self.y, use_solver=True)
             self.para['coef'] = beta
             self.para['intercept'] = beta_0
         elif method == 'GD':
@@ -36,10 +47,14 @@ class linear_regression:
             print('L1 WIP')
         elif method == 'L2':
             print('L2 WIP')
+        else:
+            print('Method not found')
+        # update model class
         self.method = method
+        self.residuals = self.y-self.predict(self.X)
 
     def predict(self, new_X):
-        if self.method == 'OLS':
+        if self.method == 'OLS' or self.method == 'OLS_solver':
             beta = self.para['coef']
             beta_0 = self.para['intercept']
             y = new_X @ beta + beta_0

daaf13dcc66ea41393e5efd0545f85682b7f73b6
Tongtian Fan <58601289+tfan666@users.noreply.github.com>
2022-07-28 15:14:40 -0400

    Update stats_modules.py
    add "confidence intervals" and "prediction intervals"

--- stats_modules.py
+++ stats_modules.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from scipy.linalg import solve
+from scipy.stats import t
+from joblib import delayed, Parallel
 
 def least_square(X, y, use_solver=False):
     if use_solver == False:
@@ -32,11 +34,20 @@ def score_test_interval(p_hat, Z, p_0, n):
 class linear_regression:
     "This class stores methods for linear regressions"
     def __init__(self, X, y):
-        self.X = X
+        if isinstance(X, np.ndarray) == True:
+            self.X = X
+        else:
+            self.X = np.array(X)
+        
         self.y = y
         self.para = {}
         self.method = None
         self.residuals = None
+        self.RSS = None
+        self.r_squared = None
+        self.TSS = None
+        self.sd_resid = None
+        self.n = len(y)
     
     def fit(self, method='OLS', _lambda=1):
         """
@@ -69,10 +80,56 @@ class linear_regression:
         # update model class
         self.method = method
         self.residuals = self.y-self.predict(self.X)
-    def predict(self, new_X):
+        self.RSS = ((self.residuals)**2).sum()
+        self.TSS = ((self.y - self.y.mean())**2).sum()
+        self.r_squared = 1 - self.RSS/self.TSS
+        self.sd_resid = (self.RSS/(self.n-2))**.5
+
+    def predict(self, new_X, interval=None, level=0.95, n_jobs=1):
         beta = self.para['coef']
         y = new_X @ beta
-        return y
+        if interval == None:
+            return y
+        
+        y_out = {
+            'mean': [],
+            'lo': [],
+            'hi': []
+        }
+        alpha = 1-level
+        critical_value = t.ppf(1-alpha/2, self.n-2)
+
+        def report(i, interval):
+            x0 = np.matrix(new_X)[i]
+            X = self.X
+            if interval == 'confidence':
+                SE_ = np.float64(np.sqrt(self.sd_resid**2 * x0 @ np.linalg.inv(X.T @ X) @ x0.T))
+            if interval == 'prediction':
+                SE_ = np.float64(np.sqrt(1 + self.sd_resid**2 * x0 @ np.linalg.inv(X.T @ X) @ x0.T))
+            hi = y[i] + critical_value*SE_
+            lo = y[i] - critical_value*SE_
+            report = {
+                'mean': y[i],
+                'lo': lo,
+                'hi': hi
+            }
+            return report
+
+        if interval=='confidence':
+            r = Parallel(n_jobs=n_jobs, verbose=0)(
+            delayed(report)(
+                i, interval = 'confidence') 
+                for i in range(len(new_X)) 
+                )
+
+        if interval=='prediction':
+            r = Parallel(n_jobs=n_jobs, verbose=0)(
+            delayed(report)(
+                i, interval = 'prediction') 
+                for i in range(len(new_X)) 
+                )
+
+        return pd.DataFrame(r)
 
 def poisson(x, _lambda):
         fx = _lambda**x * np.exp(1)**(-_lambda) / np.math.factorial(x)

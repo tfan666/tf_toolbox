8f7c1b9299034ad9044229fb4f37f19eaede0658
Tongtian Fan <franktongtim@gmail.com>
2022-08-23 23:44:41 -0400

    add k-fold cv with resampling class

--- cv.py
+++ cv.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pandas 
+import pandas as pd 
 
 def merge_array(a,b):
     """
@@ -46,3 +46,36 @@ def generate_k_fold_index(X, y, k, shuffle=True, stratified=False, random_state=
                     
 
     return cv_dict
+
+def get_cv_results(k, X, y, model, eval_func, random_state=1, clf_threshold=.5, stratified=True):
+    """
+    This method run k-fold cross-validation with resampling class. 
+    """
+
+    kfold_idx = generate_k_fold_index(X=X, y=y, k=k, stratified=stratified)
+    def _get_cv_results(i, kfold_idx=kfold_idx, X=X, y=y, model=model, eval_func=eval_func, random_state=random_state, clf_threshold=clf_threshold):
+        k_idx = kfold_idx['idx'][i]
+        _k_idx = np.array(list(set(y.index.values) - set(k_idx)))
+        k_X_train, k_y_train, k_X_test, k_y_test = X.iloc[_k_idx], y[_k_idx], X.iloc[k_idx], y[k_idx]
+        rs_k_X_train, rs_k_y_train = random_resample(X=k_X_train, y=k_y_train, target_cnt=None, random_state=1)
+
+        model.fit(X=rs_k_X_train, y=rs_k_y_train)
+
+        k_y_pred = model.predict_proba(k_X_test)[::,1]
+
+        score = eval_func(y_true=k_y_test, y_pred=np.where(k_y_pred>clf_threshold,1,0))
+
+        eval_report = {
+            'k': i,
+            'score': score
+        }
+        return eval_report
+
+    r = Parallel(n_jobs=-2)(delayed(_get_cv_results) (
+        i, kfold_idx=kfold_idx, X=X, y=y, model=model, eval_func=f2_score) 
+        for i in range(k)
+        )
+
+    cv_report = pd.DataFrame(r)
+
+    return cv_report

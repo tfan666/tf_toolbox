6907480251186c1322a9d76da3425ce05524e496
Tongtian Fan <franktongtim@gmail.com>
2022-09-22 11:43:03 -0400

    fix bugs

--- imputation.py
+++ imputation.py
@@ -18,14 +18,15 @@ def MICE(df, estimator, first_fill='mean', iterations=10, return_type='data'):
         df_ = df.fillna(df.median()).copy()
     for __ in range(iterations):
         for col in df.columns:
-            col_missing_idx = df[df[col].isnull()].index.values
-            subset = df_.iloc[df.index.difference(col_missing_idx)]
-            X_train, y_train = subset.drop(columns=col), subset[col]
-            X_test = df_.drop(columns=col).iloc[col_missing_idx]
-            imputer = estimator()
-            imputer.fit(X=np.array(X_train), y=np.array(y_train))
-            y_pred = imputer.predict(X_test)
-            df_[col].loc[col_missing_idx] = y_pred
+            if df[col].isnull().sum() > 0:
+                col_missing_idx = df[df[col].isnull()].index.values
+                subset = df_.iloc[df.index.difference(col_missing_idx)]
+                X_train, y_train = subset.drop(columns=col), subset[col]
+                X_test = df_.drop(columns=col).iloc[col_missing_idx]
+                imputer = estimator()
+                imputer.fit(X=np.array(X_train), y=np.array(y_train))
+                y_pred = imputer.predict(X_test)
+                df_[col].loc[col_missing_idx] = y_pred
         loss = df0 - df_
     if return_type == 'data':
         return df_

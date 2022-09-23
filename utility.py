028dd641c47bdd62225f7a31cb5a13390de43106
Tongtian Fan <58601289+tfan666@users.noreply.github.com>
2022-09-23 15:26:02 -0400

    Update utility.py
    moved gini index to metric.py

--- utility.py
+++ utility.py
@@ -1,22 +1,6 @@
 import pandas as pd
 import numpy as np
 
-def gini(X:np.ndarray or list):
-    """
-    Compute Gini Index 
-    X: can be either numpy array or python list
-    """
-    if isinstance(X, list):
-        X = np.array(X)
-
-    K = np.unique(X)
-    cnt = len(X)
-    gini = 1
-    for k in K:
-        pk = np.sum(([X==k]))/cnt
-        gini -= pk ** 2 
-    return gini
-
 def get_day_of_week(date, output_string = False):
     if output_string == False:
         return pd.Timestamp(date).isoweekday()
